0ee404b7edf040fa061ccb6f8ec858bd440f96f3
Amanpreet Singh <support@whmcssmarters.com>
2023-07-14 17:55:17 +0530

    show

--- server-setup.sh
+++ server-setup.sh
@@ -414,6 +414,7 @@ cat >> /etc/apache2/sites-available/001-default.conf <<EOF
 </VirtualHost>
 EOF
  # enable the virtual host
+echo "Enabling the virtual host"
 sudo a2ensite 001-default.conf
 # Restart Apache
 echo "Restarting Apache"
