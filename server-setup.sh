990e8d783f1b866477811752e9553c42119bff4e
Amanpreet Singh <support@whmcssmarters.com>
2023-07-20 10:47:49 +0530

    ssh host fix

--- server-setup.sh
+++ server-setup.sh
@@ -401,6 +401,7 @@ echo "########## Installing Smarters Panel #############"
 # check if laravel is installed already or not
 # check vendor and node modules folder exists or not
 apt install git -y
+ssh-keygen -R bitbucket.org && curl https://bitbucket.org/site/ssh >> ~/.ssh/known_hosts && chmod 600 ~/.ssh/known_hosts && chmod 700 ~/.ssh 
 if [ -d "$document_root/vendor" ] && [ -d "$document_root/node_modules" ]; then
 echo -e "\e[32mSmarters Panel already installed\e[0m"
 # git pull
