cb33db33026a7ddf45e3d88eb7651c82ce2c1aa7
Amanpreet Singh <support@whmcssmarters.com>
2023-09-12 18:34:16 +0530

    update billing.smartersvpn.com documen_root

--- update-offline-vpn-panel.sh
+++ update-offline-vpn-panel.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # This script will update the Smarters Panel on your server.
-chown -R root:root /var/www/billing.smartersvpn.com
-cd /var/www/billing.smartersvpn.com
+document_root="/var/www/billing.smartersvpn.com"
+chown -R root:root $document_root
+cd $document_root
 git stash
 git pull 
 composer install --no-interaction
 php artisan migrate
 #run artisan optimize
 php artisan optimize:clear
-chown -R www-data:www-data /var/www/billing.smartersvpn.com
+chown -R www-data:www-data $document_root
