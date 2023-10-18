e7957639eaa61286f6b76596ee6c2be581cd0f91
Amanpreet Singh <support@whmcssmarters.com>
2023-10-18 14:48:08 +0530

    dynamic root path

--- update-offline-vpn-panel.sh
+++ update-offline-vpn-panel.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # This script will update the Smarters Panel on your server.
-document_root="<give-path-here>"
+# $1 = Document root path
+# $1 is empty then it will take /var/www/html as document root
+document_root=$1
+if [ -z "$document_root" ]
+then
+     echo "Document root path is empty. please provide the document root path as first argument"
+     exit 1
+fi
 chown -R root:root $document_root
 cd $document_root
 git stash
